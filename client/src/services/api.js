e79b5cf119e27fc1238f8609f8af0c754cc8f0c1
Tesla-sudo <fredrickokwomboli@gmail.com>
2025-11-20 14:56:39 +0300

    changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const API = axios.create({ 
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/auth' });
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
